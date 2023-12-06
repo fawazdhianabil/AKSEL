addf74e4a2d3a3e4720c18d21d0ada1425f12e08
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2023-12-06 10:44:58 +0700

    Update Nabil_01810.py

--- Nabil_01810.py
+++ Nabil_01810.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from google_play_scraper import Sort, reviews_all
 from nlp_id.lemmatizer import Lemmatizer
 
-import regex as re
+import re
 import matplotlib.pyplot as plt
 import csv
 import sys
@@ -22,6 +22,8 @@ if not sys.warnoptions:
 import nltk
 from nltk.corpus import words
 nltk.download('words')
+
+from wordcloud import WordCloud
 
 # stopoword bahasa Indonesia
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -135,7 +137,7 @@ def sentimen(df):
   df['Final_Cek'] = df['Final_Cek'].str.replace('engtidak', 'tidak')
   df['Final_Cek'] = df['Final_Cek'].str.replace('aplikasix', 'aplikasi')
   df['Final_Cek'] = df['Final_Cek'].str.replace('ss', 'cuplikan')
-  df['Final_Cek'] = df['Final_Cek'].str.replace('baharu', 'update')
+  df['Final_Cek'] = df['Final_Cek'].str.replace('baharu', 'baru')
 
   df['Final_Cek'] = df['Final_Cek'].drop_duplicates().apply(split_word)
   df = df.dropna()
@@ -187,13 +189,85 @@ def sentimen(df):
 
   return df
 
+def pos(kata_positif):
+    # SENTIMEN positif
+    
+    positif_kata = kata_positif.value_counts().nlargest(20)
+     
+    positif_x = positif_kata.index
+    positif_y = positif_kata.values
+
+    fig = plt.figure(figsize = (12, 10))
+    plt.bar(positif_x, positif_y)
+    plt.xlabel("Kata", fontweight ='bold')
+    plt.ylabel("Frekuensi", fontweight ='bold')
+    plt.title("Kata pada Sentimen Positive", fontweight ='bold', fontsize = 14)
+
+    plt.xticks(positif_x, rotation = 30)
+
+    st.pyplot(fig)
+
+def neg(kata_negatif):
+    # SENTIMEN negatif
+    
+    negatif_kata = kata_negatif.value_counts().nlargest(20)
+     
+    negatif_x = negatif_kata.index
+    negatif_y = negatif_kata.values
+
+    fig = plt.figure(figsize = (12, 10))
+    plt.bar(negatif_x, negatif_y)
+    plt.xlabel("Kata", fontweight ='bold')
+    plt.ylabel("Frekuensi", fontweight ='bold')
+    plt.title("Kata pada Sentimen Negative", fontweight ='bold', fontsize = 14)
+
+    plt.xticks(negatif_x, rotation = 30)
+
+    st.pyplot(fig)
+
+def wordcloud(df):
+    positive_review = df[df['polarity'] == 'positive']
+    positive_words = positive_review['Untokenizing'].apply(split_word)
+    
+    fig, ax = plt.subplots(1,2, figsize = (15, 10))
+    list_words_postive=''
+    for row_word in positive_words:
+        for word in row_word:
+            list_words_postive += ' '+(word)
+    wordcloud_positive = WordCloud(width = 800, height = 600, background_color = 'black', colormap = 'Greens'
+                               , min_font_size = 10).generate(list_words_postive)
+    ax[0].set_title('Word Cloud dari Kata Positive AKSEL', fontsize = 14)
+    ax[0].grid(False)
+    ax[0].imshow((wordcloud_positive))
+    fig.tight_layout(pad=0)
+    ax[0].axis('off')
+
+    negative_review = df[df['polarity'] == 'negative']
+    negative_words = negative_review['Untokenizing'].apply(split_word)
+
+    list_words_negative=''
+    for row_word in negative_words:
+        for word in row_word:
+            list_words_negative += ' '+(word)
+    wordcloud_negative = WordCloud(width = 800, height = 600, background_color = 'black', colormap = 'Reds'
+                               , min_font_size = 10).generate(list_words_negative)
+    ax[1].set_title('Word Cloud dari Kata Negative AKSEL', fontsize = 14)
+    ax[1].grid(False)
+    ax[1].imshow((wordcloud_negative))
+    fig.tight_layout(pad=0)
+    ax[1].axis('off')
+    st.pyplot(fig)
+
+
+
 # code untuk streamlit
 st.title('Analisis Sentimen')
 
 with st.sidebar :
-    selected = option_menu('Menu Analisis',
+    selected = option_menu('Main Menu',
                            ['Crawling Data Playstore',
-                            'Analisis Sentimen by Lexicon'],
+                            'Analisis Sentimen by Lexicon',
+                            'Statistic by Lexicon'],
                             default_index=0)
 
 if (selected=='Crawling Data Playstore'):
@@ -206,7 +280,7 @@ if (selected=='Crawling Data Playstore'):
         result = reviews_all(alamat,
                              lang='id',
                              country='id',
-                             sort=Sort.NEWEST)
+                             sort=Sort.nEST)
         
         st.write('Crawling Data Berhasil!',pd.DataFrame(result))
 
@@ -215,17 +289,45 @@ if (selected=='Analisis Sentimen by Lexicon'):
 
     alamat_ = st.text_input("Masukan Alamat Aplikasi","")
     proses_analisis = st.button('Proses Analisis')
+    
 
-    if proses_analisis:        
+    if proses_analisis:
         df = scrap(alamat=alamat_)
         df_n = sentimen(df)
-        st.write(f'Sentimen Analisis Berhasil!',df_n)
-        fig, ax = plt.subplots(figsize = (6, 6))
         sizes = [count for count in df_n['polarity'].value_counts()]
         labels = list(df_n['polarity'].value_counts().index)
+        st.write('Sentimen Analisis Berhasil!',df_n)
+        st.write('='*88)
+        st.write('Ringkasan Data :')
+        st.write('Data Sebelum Text Preprocessing :',df.shape[0])
+        st.write('Data Sesudah Text Preprocessing :',df_n.shape[0])
+        st.write('Jumlah Sentiment Negative :',sizes[0])
+        st.write('Jumlah Sentiment Positive :',sizes[1])
+
+        fig, ax = plt.subplots(figsize = (6, 6))
         explode = (0.1, 0)
         colors = ['#66b3ff', '#ffcc99']
         ax.pie(x = sizes, labels = labels, colors=colors, autopct = '%1.1f%%', explode = explode, textprops={'fontsize': 14})
         ax.set_title('Sentiment Polarity Pada Data AKSEL', fontsize = 16, pad = 20)
         st.write('='*88)
         st.pyplot(fig)
+        st.write('='*88)
+        wordcloud(df_n)
+
+
+if (selected=='Statistic by Lexicon'):
+   st.title('Statistic by Lexicon')
+
+   alamat_ = st.text_input("Masukan Alamat Aplikasi","")
+   proses_statistik = st.button('Cek Statistik')
+
+   if proses_statistik:
+        df = scrap(alamat=alamat_)
+        df_n = sentimen(df)
+        kata_positif = pd.Series(" ".join(df_n[df_n["polarity"] == 'positive']["Untokenizing"].astype("str")).split())
+        kata_negatif = pd.Series(" ".join(df_n[df_n["polarity"] == 'negative']["Untokenizing"].astype("str")).split())
+        pos(kata_positif)
+        neg(kata_negatif)
+        
+    
+
