54a76cd8a5c2a5430c4a18d772c68d50a8df43d4
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2023-12-08 14:50:19 +0700

    Update Nabil_01810.py

--- Nabil_01810.py
+++ Nabil_01810.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+from fpdf import FPDF
+from datetime import datetime,timezone
+from zoneinfo import ZoneInfo
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -335,52 +340,110 @@ if (selected=='Analisis Sentimen by Lexicon'):
 
     al = st.selectbox('Silahkan Pilih Aplikasi',('AKSEL','Merchant Mobile (QRIS)','IBB Mobile'),
                           index=None,placeholder='Pilih')
+    pil =  st.selectbox('Simpan Sebagai...',('PDF','Tidak Menyimpan Report'),
+                          index=None,placeholder='Pilih')
     
-    proses_analisis = st.button('Proses Analisis')
-    if al == 'AKSEL':
-        alamat = 'id.co.bankkalsel.mobile_banking'
-        jdl = 'AKSEL'
-    elif al == 'Merchant Mobile (QRIS)':
-        alamat = 'com.dwidasa.kalsel.mbqris.android'
-        jdl = 'Merchant Mobile (QRIS)'
-    elif al == 'IBB Mobile':
-        alamat = 'id.co.bankkalsel.mobileibb'
-        jdl = 'IBB Mobile'
-    
-    if proses_analisis:
-        try:
-           df = scrap(alamat=alamat)
-           df_n = sentimen(df)
-           sizes = [count for count in df_n['polarity'].value_counts()]
-           labels = list(df_n['polarity'].value_counts().index)
-           st.success('Sentimen Analisis Berhasil!')
-           st.write(df_n)
-           st.write('='*88)
-           st.write('Ringkasan Data :')
-           st.write('Data Sebelum Text Preprocessing :',df.shape[0])
-           st.write('Data Sesudah Text Preprocessing :',df_n.shape[0])
-           st.write('Jumlah Sentiment Negative :',sizes[0])
-           st.write('Jumlah Sentiment Positive :',sizes[1])
+    if pil == 'PDF':
+        proses_analisis = st.button('Proses Analisis')
+        if al == 'AKSEL':
+            alamat = 'id.co.bankkalsel.mobile_banking'
+            jdl = 'AKSEL'
+        elif al == 'Merchant Mobile (QRIS)':
+            alamat = 'com.dwidasa.kalsel.mbqris.android'
+            jdl = 'Merchant Mobile (QRIS)'
+        elif al == 'IBB Mobile':
+            alamat = 'id.co.bankkalsel.mobileibb'
+            jdl = 'IBB Mobile'
+        if proses_analisis:
+            try:
+                df = scrap(alamat=alamat)
+                df_n = sentimen(df)
+                sizes = [count for count in df_n['polarity'].value_counts()]
+                labels = list(df_n['polarity'].value_counts().index)
+                st.success('Sentimen Analisis Berhasil!')
+                st.write(df_n)
+                st.write('='*88)
+                st.write('Ringkasan Data :')
+                st.write('Data Sebelum Text Preprocessing :',df.shape[0])
+                st.write('Data Sesudah Text Preprocessing :',df_n.shape[0])
+                st.write('Jumlah Sentiment Negative :',sizes[0])
+                st.write('Jumlah Sentiment Positive :',sizes[1])
 
-           fig, ax = plt.subplots(figsize = (6, 6))
-           explode = (0.1, 0)
-           colors = ['#66b3ff', '#ffcc99']
-           ax.pie(x = sizes, labels = labels, colors=colors, autopct = '%1.1f%%', explode = explode, textprops={'fontsize': 14})
-           ax.set_title(f'Sentiment Polarity Pada Data {jdl}', fontsize = 16, pad = 20)
-           st.write('='*88)
-           st.pyplot(fig)
-           st.write('='*88)
-           wordcloud(df_n,jdl)
-           st.write('='*88)
-           kata_positif = pd.Series(" ".join(df_n[df_n["polarity"] == 'positive']["Untokenizing"].astype("str")).split())
-           kata_negatif = pd.Series(" ".join(df_n[df_n["polarity"] == 'negative']["Untokenizing"].astype("str")).split())
-           pos(kata_positif,jdl)
-           st.write('='*88)
-           neg(kata_negatif,jdl)
-           st.write('='*88)
-           jam(df,jdl)
-           st.write('='*88)
-           bulan(df,jdl)
-        except:
-           st.error('Data Tidak Ditemukan',icon='🚨')
-           st.write('Hal ini Disebabkan Belum Ada Ulasan')
+                fig, ax = plt.subplots(figsize = (6, 6))
+                explode = (0.1, 0)
+                colors = ['#66b3ff', '#ffcc99']
+                ax.pie(x = sizes, labels = labels, colors=colors, autopct = '%1.1f%%', explode = explode, textprops={'fontsize': 14})
+                ax.set_title(f'Sentiment Polarity Pada Data {jdl}', fontsize = 16, pad = 20)
+                st.write('='*88)
+                st.pyplot(fig)
+                st.write('='*88)
+                wordcloud(df_n,jdl)
+                st.write('='*88)
+                kata_positif = pd.Series(" ".join(df_n[df_n["polarity"] == 'positive']["Untokenizing"].astype("str")).split())
+                kata_negatif = pd.Series(" ".join(df_n[df_n["polarity"] == 'negative']["Untokenizing"].astype("str")).split())
+                pos(kata_positif,jdl)
+                st.write('='*88)
+                neg(kata_negatif,jdl)
+                st.write('='*88)
+                jam(df,jdl)
+                st.write('='*88)
+                bulan(df,jdl)
+
+                j = int(datetime.now(ZoneInfo('Asia/Jakarta')).strftime("%H"))
+                hari = datetime.now(ZoneInfo('Asia/Jakarta')).strftime("%m/%d/%Y")
+                wkt = datetime.now(ZoneInfo('Asia/Jakarta')).strftime(f"{j+1}:%M:%S")
+                WIDTH =210
+                HEIGHT = 297
+                pdf = FPDF()
+                #hal pertama
+                pdf.add_page()
+                pdf.set_font('Arial','B',16)
+                pdf.ln(5)
+                pdf.write(5,f'Analisis Sentimen {jdl}')
+                pdf.ln(7)
+                pdf.set_font('Arial','B',12)
+                pdf.write(4,f'Tanggal : {hari}, Pukul : {wkt} WITA')
+                pdf.image("apk.jpg",0,30,WIDTH/2-5)
+                pdf.image("Polarity.jpg",WIDTH/2+5,30,WIDTH/2-5)
+                pdf.image("wc_positive.jpg",5,120,WIDTH/2-10,HEIGHT/5+5)
+                pdf.image("wc_negative.jpg",WIDTH/2+5,120,WIDTH/2-10,HEIGHT/5+5)
+                pdf.image("Positive.jpg",0,190,WIDTH/2-5,HEIGHT/5+5)
+                pdf.image("negative.jpg",WIDTH/2+5,190,WIDTH/2-5,HEIGHT/5+5)
+
+                #hal kedua
+                pdf.add_page()
+                pdf.set_font('Arial','B',16)   
+                pdf.ln(5)
+                pdf.write(5,f'Ringkasan Statistik {jdl}')
+                pdf.ln(7)
+                pdf.set_font('Arial','B',12)
+                pdf.write(4,f'Tanggal : {hari}, Pukul : {wkt} WITA')
+                pdf.image("Jam.jpg",0,30,WIDTH/2-5,HEIGHT/5+5)
+                pdf.image("Bulan.jpg",WIDTH/2+5,30,WIDTH/2-5,HEIGHT/5+5)
+
+                #ringkasan
+                pdf.set_font('Arial','B',12)
+                pdf.ln(80)
+                pdf.write(5,'='*76)
+                pdf.ln(10)
+                pdf.write(5,'Ringkasan Data :')
+                pdf.ln(10)
+                pdf.write(1,f'Data Sebelum Text Preprocessing :{d.shape[0]}')
+                pdf.ln(6)
+                pdf.write(1,f'Data Sesudah Text Preprocessing :{df_new.shape[0]}')
+                pdf.ln(6)
+                pdf.write(1,f'Jumlah Sentiment Negative :{sizes[0]}')
+                pdf.ln(6)
+                pdf.write(1,f'Jumlah Sentiment Positive :{sizes[1]}')
+
+                pdf.output(f'Sentimen Analisis {jdl}.pdf','F')
+                with open(f"Sentimen ANalisis {jdl}", "rb") as pdf_file:
+                    PDFbyte = pdf_file.read()
+
+                st.download_button(label="Export_Report",
+                                   data=PDFbyte,
+                                   file_name="test.pdf",
+                                   mime='application/octet-stream')
+            except Exception as e:
+                st.error(e)
+                st.write('Hal ini Disebabkan Belum Ada Ulasan')
