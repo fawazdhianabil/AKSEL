27346d1d5af7855b887eb327986f0b1e60d22083
Muhammad Athanabil Andi Fawazdhia <80562205+fawazdhianabil@users.noreply.github.com>
2023-12-24 22:25:19 +0700

    Update Nabil_01810.py

--- Nabil_01810.py
+++ Nabil_01810.py
@@ -579,8 +579,8 @@ if (selected=='Analisis Sentimen by Lexicon'):
         proses_analisis = st.button('Proses Analisis')
         if proses_analisis:
             try:
-                st.dataframe(df)
-                df_n = sentimen(df)
+                st.dataframe(df[['userName','content','score','at','reviewCreatedVersion','appVersion']])
+                df_n = sentimen(df[['userName','content','score','at','reviewCreatedVersion','appVersion']])
                 sizes = [count for count in df_n['polarity'].value_counts()]
                 labels = list(df_n['polarity'].value_counts().index)
                 j = int(datetime.now(ZoneInfo('Asia/Jakarta')).strftime("%H"))
